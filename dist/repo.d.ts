e8ca50bf93ee60c6c573f46849edb313f9f5a704
irohitb <irohitbhatia@gmail.com>
2024-05-26 03:27:19 +0530

    fix: add dist to gitignore and remove files

--- dist/repo.d.ts
+++
@@ -1,2 +0,0 @@
-export declare function getModifiedSqlFiles(sourcePath: string, diffBase: string): Promise<string[]>;
-export declare function getMigrationFiles(supabaseDir: string, diffBase: string): Promise<string[]>;
--- dist/watch.d.ts
+++
@@ -1,7 +0,0 @@
-interface WatchArgs {
-    sourcePath: string;
-    fileChanged: (path: string) => void;
-    log: (message?: any, ...optionalParams: any[]) => void;
-}
-export declare function watchPath({ sourcePath, fileChanged, log }: WatchArgs): void;
-export {};
